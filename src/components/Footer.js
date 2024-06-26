decc3de48625a81f6b5c7b888d538c3ab8d07315
Muskan-Yadav-369 <148032461+Muskan-Yadav-369@users.noreply.github.com>
2024-06-26 17:33:59 +0530

    Update Footer.js
    Added linkedin link and uploaded resume

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import "./footer.css"
 const Footer = () => {
 
   const year = new Date().getFullYear()
-
+  const resumePDF = process.env.PUBLIC_URL + '/resume.pdf'; 
   return (
     <>
       <div className="footer">
@@ -12,20 +12,25 @@ const Footer = () => {
           <div className="first mt-5">
             <h4>Muskan Yadav</h4>
             <p>© {year} Muskan Yadav All rights reserved</p>
-            <p className='d-flex'>
-              <i className='fa-brands fa-instagram'></i>
-              <i className='fa-brands fa-facebook mx-3'></i>
+           <p className='d-flex'>
+              <a href="https://www.instagram.com/shivanshi_404/" target="_blank" rel="noopener noreferrer">
+                <i className='fab fa-instagram mr-3'></i> {/* Added mr-3 for margin-right */}
+              </a>
+              <a href="https://www.linkedin.com/in/muskan-yadav-967110272/" target="_blank" rel="noopener noreferrer">
+                <i className='fab fa-linkedin'></i>
+              </a>
             </p>
           </div>
           <div className="second mt-5">
             <h4>Get In Touch</h4>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore, adipisci.</p>
+            <p>Contact me for collaborations.</p>
             <p>info@muskanyadav.com</p>
             <p>+91 012345678</p>
           </div>
           <div className="third mt-5">
             <h4>About</h4>
-            <p>Resume</p>
+             <p><a href={resumePDF} target="_blank" rel="noopener noreferrer">Resume</a></p>
+
           </div>
         </div>
       </div>
